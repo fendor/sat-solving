c9bf68630083e3f9d257255665fae0c032e1cc4d
Fendor <power.walross@gmail.com>
2020-09-23 17:32:42 +0200

    Add basic benchmark system

---
+++ benchmarks/Bench.hs
@@ -0,0 +1,42 @@
+import Control.Monad (forM)
+import Criterion.Main
+import RandomSat
+import System.Random
+import UnsatCore
+
+-- Our benchmark harness.
+main :: IO ()
+main = do
+  let stdGen = mkStdGen 45
+  let gens = randomGens stdGen
+  let cnfs clauseLen variables clauseNum gen =
+        let randomCnfConfig =
+              RandomCnfConfig
+                { randomConfigClauseLength = clauseLen,
+                  randomConfigNumVars = variables,
+                  randomConfigNumClauses = clauseNum,
+                  randomConfigComment = []
+                }
+         in randomCnf randomCnfConfig gen
+  let configs =
+        map (\(cnfConfig, gen) -> (cnfConfig, uncurry3 cnfs cnfConfig gen)) $
+          zip
+            [ (3, 10, 40),
+              (3, 100, 400),
+              (4, 100, 400),
+              (3, 500, 2000),
+              (4, 500, 2000)
+            ]
+            gens
+
+  benchmarkGroups <- forM configs $ \((clauseLen, variables, clauseNum), cnf) -> do
+    pure $
+      bgroup
+        ("Random Cnf " ++ show (clauseLen, variables, clauseNum))
+        [ bench "sat" $ whnfIO (findUnsatCore cnf)
+        ]
+
+  defaultMain benchmarkGroups
+
+uncurry3 :: (t1 -> t2 -> t3 -> t4) -> (t1, t2, t3) -> t4
+uncurry3 f (a, b, c) = f a b c
