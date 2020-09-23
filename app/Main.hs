3d611ce017c6f93a040eace8f343f720d19ed498
Fendor <power.walross@gmail.com>
2020-09-23 17:30:26 +0200

    Add basic runner for QBF reduction

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,17 @@
 module Main where
 
+import RandomSat
+import System.Random
+import QBFUnsatCore
+import CnfPrint
+import qualified Data.Text.IO as T
+
 main :: IO ()
-main = putStrLn "Hello, World"
+main = do
+  let stdGen = mkStdGen 45
+  let randomCnfConfig = RandomCnfConfig 3 4 5 []
+  let cnf = randomCnf randomCnfConfig stdGen
+
+  T.putStrLn $ dimacsText cnf
+  putStrLn $ reduceToQCir cnf
+  pure ()
